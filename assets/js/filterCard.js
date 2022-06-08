7aa27b207a227f0436d2086dba7e6ce060f1afca
badr ghanbiE <bghaE@gmail.com>
2022-06-08 23:11:12 +0100

    change the background color of the target btns

--- assets/js/filterCard.js
+++ assets/js/filterCard.js
@@ -1,7 +1,12 @@
 const workBtns=document.querySelectorAll('.work__btn');
 const workCards=document.querySelectorAll('.work__card');
 
-workBtns.forEach(btn=>btn.addEventListener('click',()=>{
+workBtns.forEach(btn=>btn.addEventListener('click',(e)=>{
+    // change the background color of the current active btn
+     workBtns.forEach(element=>btnRemoveBg(element))
+        e.target.classList.add('work__btn--active');
+
+        // filter the cards according to the id of the clicked btn
         let currentAttribute=btn.getAttribute('data-type');
         workCards.forEach(card=>{
              if (btn.id==='All'){
@@ -22,4 +27,8 @@ function showCards(card){
 }
 function hideCards(card){
     card.classList.add('hide-card');
+}
+
+function btnRemoveBg(entry){
+   entry.classList.remove('work__btn--active');
 }
