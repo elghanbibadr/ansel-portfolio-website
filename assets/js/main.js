8562b715cfc5b0e8c610697c15ef296990247f03
badr ghanbiE <bghaE@gmail.com>
2022-06-10 11:55:34 +0100

    starter styles for the testomonial section got stuck i'll complete when finishing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 import './nav.js';
 import './modal.js';
 import './filterCard.js';
+
+let slides=document.querySelectorAll('.slider-cards');
+
+slides.forEach((slide,i)=>{
+    slide.style.transform=`translateX(${i * 110}%)`
+})
