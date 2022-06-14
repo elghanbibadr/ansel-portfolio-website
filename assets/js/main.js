28030ba1a135318a5f588d5df8f29ff79e4f86ab
badr ghanbiE <bghaE@gmail.com>
2022-06-14 14:22:29 +0100

    organize the workSpace by putting some styles to their own stylesheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 
- import './nav.js';
- import './fixedNav.js';
- import './modal.js';
- import './filterCard.js';
- import './carousel.js';
+import './nav.js';
+import './fixedNav.js';
+import './modal.js';
+import './filterCard.js';
+import './carousel.js';
 import './_ThemeChange.js';
-
+import './scroll.js';
